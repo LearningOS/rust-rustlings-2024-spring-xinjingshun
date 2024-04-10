3f101c486dcad6611b944601dfb54d67221806e6
xinjingshun <xinjingshun@foxmail.com>
2024-04-10 23:58:08 +0800

    Options_is_OK

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM  DONE
 
 struct Point {
     x: i32,
@@ -14,7 +14,7 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y), //创建一个引用,避免y不可用。
         _ => panic!("no match!"),
     }
     y; // Fix without deleting this line.
