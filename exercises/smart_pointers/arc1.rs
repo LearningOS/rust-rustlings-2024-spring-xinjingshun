fb25b959f7380366b197757284cd1d4ff169769c
xinjingshun <xinjingshun@foxmail.com>
2024-04-23 02:50:25 +0800

    Cow_is_OK

--- exercises/smart_pointers/arc1.rs
+++ exercises/smart_pointers/arc1.rs
@@ -1,27 +1,20 @@
 // arc1.rs
 //
-// In this exercise, we are given a Vec of u32 called "numbers" with values
-// ranging from 0 to 99 -- [ 0, 1, 2, ..., 98, 99 ] We would like to use this
-// set of numbers within 8 different threads simultaneously. Each thread is
-// going to get the sum of every eighth value, with an offset.
+// 在这个练习中，我们有一个包含从 0 到 99 的值的名为 "numbers" 的 Vec<u32> — [0, 1, 2, ..., 98, 99]。我们想要在 8 个不同的线程中同时使用这组数字。每个线程将对具有偏移量的每八个值进行求和。
 //
-// The first thread (offset 0), will sum 0, 8, 16, ...
-// The second thread (offset 1), will sum 1, 9, 17, ...
-// The third thread (offset 2), will sum 2, 10, 18, ...
+// 第一个线程（偏移量为 0）将求和 0、8、16、...
+// 第二个线程（偏移量为 1）将求和 1、9、17、...
+// 第三个线程（偏移量为 2）将求和 2、10、18、...
 // ...
-// The eighth thread (offset 7), will sum 7, 15, 23, ...
+// 第八个线程（偏移量为 7）将求和 7、15、23、...
 //
-// Because we are using threads, our values need to be thread-safe.  Therefore,
-// we are using Arc.  We need to make a change in each of the two TODOs.
+// 因为我们使用了线程，所以我们的值需要是线程安全的。因此，我们使用了 Arc。我们需要在两个 TODO 中做出更改。
 //
-// Make this code compile by filling in a value for `shared_numbers` where the
-// first TODO comment is, and create an initial binding for `child_numbers`
-// where the second TODO comment is. Try not to create any copies of the
-// `numbers` Vec!
+// 在第一个 TODO 的注释中填入 shared_numbers 的值，使得该代码能够编译通过，并在第二个 TODO 注释中为 child_numbers 创建初始绑定。尽量不要创建 numbers 的任何副本！
 //
 // Execute `rustlings hint arc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #![forbid(unused_imports)] // Do not change this, (or the next) line.
 use std::sync::Arc;
@@ -29,17 +22,18 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
-    let mut joinhandles = Vec::new();
+    let shared_numbers = Arc::new(numbers);// 创建一个 Arc，使其拥有 numbers 的所有权，并将其存储在堆上。Arc 允许多个线程共享所有权，因为 `numbers` 会被多个线程共享，所以我们使用了 Arc。
+    let mut joinhandles = Vec::new();   // 创建一个用于存储线程句柄的 Vec
 
     for offset in 0..8 {
-        let child_numbers = // TODO
-        joinhandles.push(thread::spawn(move || {
+        let child_numbers = Arc::clone(&shared_numbers);// 使用 Arc::clone 创建了一个新的 Arc 指针，将其与 shared_numbers 指向的堆上的数据共享。这个 Arc 的引用计数被递增，以便将其传递给线程。
+        joinhandles.push(thread::spawn(move || {  // 启动一个新线程并将其推入 joinhandles 中。这个线程会获取 child_numbers 的所有权。
+            // 计算偏移量所对应的索引，然后将每个第 8 个值相加。
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
             println!("Sum of offset {} is {}", offset, sum);
         }));
     }
     for handle in joinhandles.into_iter() {
-        handle.join().unwrap();
+        handle.join().unwrap(); // d等待所有线程完成,如果没有这行代码，main 函数将会先于其他线程完成并退出，从而导致其他线程的工作被中断。
     }
 }
--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -1,27 +1,21 @@
 // cow1.rs
 //
-// This exercise explores the Cow, or Clone-On-Write type. Cow is a
-// clone-on-write smart pointer. It can enclose and provide immutable access to
-// borrowed data, and clone the data lazily when mutation or ownership is
-// required. The type is designed to work with general borrowed data via the
-// Borrow trait.
-//
-// This exercise is meant to show you what to expect when passing data to Cow.
-// Fix the unit tests by checking for Cow::Owned(_) and Cow::Borrowed(_) at the
-// TODO markers.
+// 本练习探讨了 Cow，或称为 Clone-On-Write 类型。Cow 是一个惰性克隆的智能指针。它可以封装并提供对借用数据的不可变访问，并在需要进行突变或拥有权时惰性克隆数据。该类型旨在通过 Borrow trait 与通用借用数据一起使用。
+
+// 本练习旨在向您展示将数据传递给 Cow 时可以期望什么。通过在 TODO 标记处检查 Cow::Owned() 和 Cow::Borrowed()，修复单元测试。
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::borrow::Cow;
 
 fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
-    for i in 0..input.len() {
+    for i in 0..input.len() { // .len()将得到input的数字的长度，比如128长度是3
         let v = input[i];
         if v < 0 {
             // Clones into a vector if not already owned.
-            input.to_mut()[i] = -v;
+            input.to_mut()[i] = -v; //当你使用 `to_mut()` 方法时，它将检查当前 `Cow` 实例是否已经是所有者实例。如果不是，则会克隆该实例到一个新的所有者实例，然后返回 mutable 的引用。
         }
     }
     input
@@ -48,31 +42,31 @@ mod tests {
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Borrowed(_) => Ok(()),
+            _ => Err("Expected borrowed value"),
         }
     }
 
     #[test]
     fn owned_no_mutation() -> Result<(), &'static str> {
-        // We can also pass `slice` without `&` so Cow owns it directly. In this
-        // case no mutation occurs and thus also no clone, but the result is
-        // still owned because it was never borrowed or mutated.
+        // 我们也可以不使用 `&` 将 `slice` 传递给 `Cow`，这样 `Cow` 就直接拥有它。 在这种情况下，没有 mutation 发生，也没有克隆，但是结果仍然是所有者的，因为从来没有被借用或修改过。
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 
     #[test]
     fn owned_mutation() -> Result<(), &'static str> {
-        // Of course this is also the case if a mutation does occur. In this
-        // case the call to `to_mut()` returns a reference to the same data as
-        // before.
+
+        //当发生mutation时，情况当然也是如此。在这种情况下，对 to_mut() 的调用将返回与之前相同的数据的引用。
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 }
