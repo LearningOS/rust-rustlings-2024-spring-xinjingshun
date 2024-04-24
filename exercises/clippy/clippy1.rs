1bda60694e986269cf6e502bc959cb5c98206f59
xinjingshun <xinjingshun@foxmail.com>
2024-04-24 10:35:06 +0800

    clippy_is_OK

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -1,23 +1,24 @@
 // clippy1.rs
 //
-// The Clippy tool is a collection of lints to analyze your code so you can
-// catch common mistakes and improve your Rust code.
+
+// Clippy 工具是一组 lint，用于分析你的代码，以便发现常见错误并改进你的 Rust 代码。
 //
-// For these exercises the code will fail to compile when there are clippy
-// warnings check clippy's suggestions from the output to solve the exercise.
+// 在这些练习中，当出现 Clippy 警告时，代码将无法编译，请检查 Clippy 的建议来解决练习。
 //
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::f32;
+use core::f32::consts::PI;
+
 
 fn main() {
-    let pi = 3.14f32;
+    // const PI:f32 = 3.14;
     let radius = 5.00f32;
 
-    let area = pi * f32::powi(radius, 2);
+    let area = PI * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
