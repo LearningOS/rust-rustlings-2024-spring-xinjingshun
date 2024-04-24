669dd7196303cd01dff214244a3a0c656d77ad18
xinjingshun <xinjingshun@foxmail.com>
2024-04-24 08:53:43 +0800

    threads_is_OK

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -1,32 +1,46 @@
 // threads1.rs
 //
-// This program spawns multiple threads that each run for at least 250ms, and
-// each thread returns how much time they took to complete. The program should
-// wait until all the spawned threads have finished and should collect their
-// return values into a vector.
+// 这个程序生成多个线程，每个线程至少运行250毫秒，
+// 并且每个线程返回它们完成所需的时间。程序应该等待所有生成的线程都完成，并将它们的返回值收集到一个向量中
 //
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
 
 fn main() {
+    // 创建一个空的线程句柄向量，用于存储每个线程的句柄。
     let mut handles = vec![];
+
+// 循环从0到9（不包括10），创建10个线程。
     for i in 0..10 {
+        // 使用thread::spawn启动一个新的线程。
+        // 每个线程的闭包参数使用move关键字，意味着闭包会获取捕获变量的所有权。
         handles.push(thread::spawn(move || {
+            // 使用Instant::now()获取当前的精确时间，作为线程开始执行的时间点。
             let start = Instant::now();
+            // 使用thread::sleep使线程休眠250毫秒。
             thread::sleep(Duration::from_millis(250));
+            // 打印一条消息，表明当前的线程（由变量i表示）已经完成。
             println!("thread {} is complete", i);
+            // 计算从线程开始到当前经过的时间，并以毫秒为单位返回。
+            // start.elapsed()获取从start到当前时间的持续时间。
+            // as_millis()将这个时间转换为毫秒。
             start.elapsed().as_millis()
         }));
     }
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        // TODO: 一个结构体从thread::spawn返回，你可以使用它吗？
+        // 使用join等待线程完成并获取返回值
+        match handle.join(){
+            Ok(Duration) => results.push(Duration),
+            Err(e) => println!("Error {:?}",e)
+        }
     }
 
     if results.len() != 10 {
--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -1,15 +1,14 @@
 // threads2.rs
 //
-// Building on the last exercise, we want all of the threads to complete their
-// work but this time the spawned threads need to be in charge of updating a
-// shared value: JobStatus.jobs_completed
+// 在上一个练习的基础上，我们希望所有线程都完成其工作，但这次生成的线程需要负责更新一个
+// 共享值：JobStatus.jobs_completed
 //
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
-use std::sync::Arc;
+use std::sync::{Arc,Mutex}; //添加互斥锁
 use std::thread;
 use std::time::Duration;
 
@@ -18,22 +17,33 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    // 使用 Arc<Mutex<JobStatus>> 来包装 JobStatus，以便线程安全地共享和修改。
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
     let mut handles = vec![];
     for _ in 0..10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            // TODO: 在更新共享值之前，您必须采取一个操作。
+            // 为了修改 jobs_completed，首先需要获取互斥锁的锁。
+            // lock 方法会阻塞当前线程直到互斥锁可用。
+            let mut num = status_shared.lock().unwrap();
+            num.jobs_completed += 1;
+            println!("jobs_completed is {:?}", num.jobs_completed)
+            // 锁在 num 离开作用域时自动释放。
         });
         handles.push(handle);
     }
     for handle in handles {
         handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
-        // anything interesting in the output? Do you have to 'join' on all the
-        // handles?
-        println!("jobs completed {}", ???);
+        // TODO: 打印 JobStatus.jobs_completed 的值。你有没有在输出中注意到什么有趣的内容？你是否需要在所有的句柄上进行 'join' 操作？
+        // 由于我们使用了互斥锁，所有线程对 jobs_completed 的增加都会是线程安全的。
+        // 下面是不加锁打印，打印的结果很可能不递增，出现连续打印相同值
+        // println!("jobs completed {:?}", status.lock().unwrap().jobs_completed);
+        // 下面是加锁打印，将只打印出最终结果10
+        println!("jobs completed {}，and locked print", {
+            let lock = status.lock().unwrap();
+            lock.jobs_completed
+        });
     }
 }
